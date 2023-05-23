daab6547434a901234fdad52e4eb14cac0dddd5a
Radicitus <novivolatus@gmail.com>
2023-05-22 18:37:00 -0700

    Add demo cards

--- Tailwind/tailwind/app/page.tsx
+++ Tailwind/tailwind/app/page.tsx
@@ -1,7 +1,20 @@
 export default function Home() {
   return (
-    <main className="flex justify-center items-center min-h-screen">
-      <button className="bg-amber-400 px-4 rounded-3xl">Hello</button>
+    <main className="flex justify-center items-center min-h-screen bg-black flex-col gap-12">
+      <div className="bg-white text-gray-700 p-4 rounded-md">
+        <h1 className="text-4xl font-bold">Hello World</h1>
+        <p className="text-gray-500">This is a sample page</p>
+      </div>
+
+      <div className="bg-white text-gray-700 p-4 rounded-md">
+        <h1 className="text-4xl font-bold">Hello World</h1>
+        <p className="text-gray-500">This is a sample page</p>
+      </div>
+
+      <div className="bg-white text-gray-700 p-4 rounded-md">
+        <h1 className="text-4xl font-bold">Hello World</h1>
+        <p className="text-gray-500">This is a sample page</p>
+      </div>
     </main>
   );
 }
